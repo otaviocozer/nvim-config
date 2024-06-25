1cfd766a2967600611c9aea2dde2c16aef35ee75
otavio <otavio@team.olhododono.agr.br>
2024-06-25 17:46:16 -0300

    init commit

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        position = "right", -- Set the position to 'right'
+        width = 30, -- You can adjust the width if needed
+      },
+    },
+  },
+}
