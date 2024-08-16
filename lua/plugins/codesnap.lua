eb80405b7863dfde1059add171c119698e73c343
OtavioCozer <otaviocozer22@gmail.com>
2024-08-16 16:31:54 -0300

    condesnap

---
+++ lua/plugins/codesnap.lua
@@ -0,0 +1,11 @@
+return {
+  "mistricky/codesnap.nvim",
+  build = "make build_generator",
+  keys = {
+    { "<leader>cl", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
+    { "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
+  },
+  opts = {
+    has_breadcrumbs = true,
+  },
+}
