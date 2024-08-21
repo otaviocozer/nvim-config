0e84e847986c3499beae2fb4b6966fb26cfcc59b
OtavioCozer <otaviocozer22@gmail.com>
2024-08-21 11:50:52 -0300

    update

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -5,6 +5,9 @@ return {
       window = {
         position = "right", -- Set the position to 'right'
         width = 30, -- You can adjust the width if needed
+        mappings = {
+          ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
+        }
       },
     },
   },
