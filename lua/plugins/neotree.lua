6dea8b09cf86da8dd31516f6b6e6893c9091ebcc
otavio <otavio@team.olhododono.agr.br>
2024-12-19 09:33:12 -0300

    open without focus

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,7 +7,12 @@ return {
         width = 30, -- You can adjust the width if needed
         mappings = {
           ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
-        }
+          ["<cr>"] = { "open", config = { focus_opened_file = false } },
+          ["<tab>"] = function(state)
+            state.commands["open"](state)
+            vim.cmd("Neotree reveal")
+          end,
+        },
       },
     },
   },
